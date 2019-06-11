57f92a2db27f5755e98dca52835420d4ae4c3080
Karishma Patel <patelkarishma10@gmail.com>
2019-06-11 21:02:26 +0100

    created user entity

--- src/main/java/com/bae/persistence/domain/User.java
+++ src/main/java/com/bae/persistence/domain/User.java
@@ -1,5 +1,21 @@
 package com.bae.persistence.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column(length = 50)
+	private String username;
+	@Column(length = 50)
+	private String password;
+	@Column(length = 200)
+	private String email;
 }
