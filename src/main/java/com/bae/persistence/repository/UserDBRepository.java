e5c23607c33ec71d0d676bdff01733d1b70015f6
Karishma Patel <patelkarishma10@gmail.com>
2019-06-11 21:23:06 +0100

    added abstract getAUser method into  user repo interface

--- src/main/java/com/bae/persistence/repository/UserDBRepository.java
+++ src/main/java/com/bae/persistence/repository/UserDBRepository.java
@@ -2,4 +2,10 @@ package com.bae.persistence.repository;
 
 public class UserDBRepository implements UserRepository {
 
+	@Override
+	public String getAUser(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
