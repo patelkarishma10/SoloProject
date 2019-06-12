79f89f5e94d3be1b50d387e9459b8441ca7b9b70
patelkarishma10 <patelkarishma10@gmail.com>
2019-06-12 10:07:01 +0100

    injected UserRepository into UserServiceImpl

--- src/main/java/com/bae/business/service/UserServiceImpl.java
+++ src/main/java/com/bae/business/service/UserServiceImpl.java
@@ -1,6 +1,13 @@
 package com.bae.business.service;
 
+import javax.inject.Inject;
+
+import com.bae.persistence.repository.UserRepository;
+
 public class UserServiceImpl implements UserService {
+
+	@Inject
+	private UserRepository repo;
 
 	// C
 
