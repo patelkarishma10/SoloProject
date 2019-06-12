a61dfaf2a2d881cc461f5061a8b566e074b46809
patelkarishma10 <patelkarishma10@gmail.com>
2019-06-12 10:19:34 +0100

    injected userService into UserController

--- src/main/java/com/bae/rest/UserController.java
+++ src/main/java/com/bae/rest/UserController.java
@@ -1,8 +1,14 @@
 package com.bae.rest;
 
+import javax.inject.Inject;
 import javax.ws.rs.Path;
+
+import com.bae.business.service.UserService;
 
 @Path("/user")
 public class UserController {
+
+	@Inject
+	private UserService service;
 
 }
