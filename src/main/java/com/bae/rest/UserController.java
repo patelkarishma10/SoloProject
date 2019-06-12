edc13811d31a1e0eb82276f957b36beeaa4464b5
patelkarishma10 <patelkarishma10@gmail.com>
2019-06-12 10:24:47 +0100

    created getAUser path in rest

--- src/main/java/com/bae/rest/UserController.java
+++ src/main/java/com/bae/rest/UserController.java
@@ -1,7 +1,10 @@
 package com.bae.rest;
 
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 import com.bae.business.service.UserService;
 
@@ -10,5 +13,12 @@ public class UserController {
 
 	@Inject
 	private UserService service;
+
+	@Path("/getAUser/{id}")
+	@GET
+	@Produces({ "application/json" })
+	public String getAUser(@PathParam("id") int id) {
+		return service.getAUser(id);
+	}
 
 }
