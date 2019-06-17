aea4cff0b19899dcb5d889394d650c313a1d5b0a
patelkarishma10 <patelkarishma10@gmail.com>
2019-06-17 15:20:07 +0100

    created getAllFilms method path in rest layer and tested in postman

--- src/main/java/com/bae/persistence/repository/FilmDBRepository.java
+++ src/main/java/com/bae/persistence/repository/FilmDBRepository.java
@@ -1,15 +1,21 @@
 package com.bae.persistence.repository;
 
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+
 import java.util.Collection;
 
+import javax.enterprise.inject.Default;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
 
 import com.bae.persistence.domain.Film;
 import com.bae.util.JSONUtil;
 
+@Transactional(SUPPORTS)
+@Default
 public class FilmDBRepository implements FilmRepository {
 
 	@Inject
