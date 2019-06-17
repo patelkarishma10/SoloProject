bf68efa37b029871122f75ef810700162d417448
patelkarishma10 <patelkarishma10@gmail.com>
2019-06-17 15:06:35 +0100

    added getAllFilms() to FilmDBReopsitory class (and created Film repo interface)

---
+++ src/main/java/com/bae/persistence/repository/FilmDBRepository.java
@@ -0,0 +1,11 @@
+package com.bae.persistence.repository;
+
+public class FilmDBRepository implements FilmRepository {
+
+	@Override
+	public String getAllFilms() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/main/java/com/bae/persistence/repository/FilmRepository.java
+++ src/main/java/com/bae/persistence/repository/FilmRepository.java
@@ -2,4 +2,5 @@ package com.bae.persistence.repository;
 
 public interface FilmRepository {
 
+	String getAllFilms();
 }
