24ad68434674154f8f8e4a23d60fb747b27fc6ab
patelkarishma10 <patelkarishma10@gmail.com>
2019-06-17 15:11:07 +0100

    created getAllFilms method

--- src/main/java/com/bae/persistence/repository/FilmDBRepository.java
+++ src/main/java/com/bae/persistence/repository/FilmDBRepository.java
@@ -1,11 +1,28 @@
 package com.bae.persistence.repository;
 
+import java.util.Collection;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import com.bae.persistence.domain.Film;
+import com.bae.util.JSONUtil;
+
 public class FilmDBRepository implements FilmRepository {
+
+	@Inject
+	private JSONUtil json;
+
+	@PersistenceContext(unitName = "primary")
+	private EntityManager em;
 
 	@Override
 	public String getAllFilms() {
-		// TODO Auto-generated method stub
-		return null;
+		TypedQuery<Film> query = em.createQuery("SELECT f FROM Film f", Film.class);
+		Collection<Film> films = query.getResultList();
+		return json.getJSONForObject(films);
 	}
 
 }
