46e1282098970cadd20b393dd936747070dfbf51
patelkarishma10 <patelkarishma10@gmail.com>
2019-06-17 15:41:07 +0100

    created UserDBRepositoryTest class

---
+++ src/test/java/com/bae/persistence/repository/UserDBRepositoryTest.java
@@ -0,0 +1,9 @@
+package com.bae.persistence.repository;
+
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserDBRepositoryTest {
+
+}
