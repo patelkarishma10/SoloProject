9bf9a81b49a5baa56e0aca523bc6fb22e5fbe414
patelkarishma10 <patelkarishma10@gmail.com>
2019-06-18 17:45:03 +0100

    created getAUserTest service test

--- src/test/java/com/bae/business/service/UserServiceImplTest.java
+++ src/test/java/com/bae/business/service/UserServiceImplTest.java
@@ -3,13 +3,17 @@ package com.bae.business.service;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import com.bae.persistence.repository.UserDBRepository;
+import com.bae.util.Constants;
 import com.bae.util.JSONUtil;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -34,6 +38,14 @@ public class UserServiceImplTest {
 		repo.setManager(manager);
 		util = new JSONUtil();
 		repo.setUtil(util);
+	}
+
+	@Test
+	public void getAUserTest() {
+		Mockito.when(service.getAUser(1)).thenReturn(Constants.MOCK_USER_OBJECT);
+
+		Assert.assertEquals(Constants.MOCK_USER_OBJECT, repo.getAUser(1));
+
 	}
 
 }
