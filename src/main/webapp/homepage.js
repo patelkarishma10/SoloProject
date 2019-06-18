d753656669c1f04b3d5499f0afb8db105af605d2
patelkarishma10 <patelkarishma10@gmail.com>
2019-06-18 16:58:16 +0100

    added in all current HTML files (fromthe SoloProjectHTML Repo)

---
+++ src/main/webapp/homepage.js
@@ -0,0 +1,54 @@
+function getUserFilms() {
+
+    let ID = sessionStorage.getItem('ID');
+    const container = document.getElementById('displayDetails');
+    makeRequest("GET", `http://localhost:8080/SoloProject/api/user/getAUser/${ID}`)
+        .then((data) => {
+
+            const containerDiv = document.getElementById('favFilmsDiv');
+            let tableHeading = document.createElement('h1');
+            tableHeading.id = "tableHeading";
+            tableHeading.innerHTML = "Your favourite films";
+            containerDiv.appendChild(tableHeading);
+            if (document.contains(document.getElementById("table1"))) {
+                containerDiv.removeChild(document.getElementById("table1"));
+                containerDiv.removeChild(document.getElementById("tableHeading"));
+            }
+            let container = document.createElement('table');
+            container.id = "table1";
+            containerDiv.appendChild(container);
+            let tableHeadingTitle = document.createElement('th');
+            tableHeadingTitle.innerHTML = "Tilte";
+            container.appendChild(tableHeadingTitle);
+            let tableHeadingRemoveFilm = document.createElement('th');
+            tableHeadingRemoveFilm.innerHTML = "Remove Film";
+            container.appendChild(tableHeadingRemoveFilm);
+
+            for (let i = 0; i < data.films.length; i++) {
+                let myRow = document.createElement('tr');
+                myRow.id = "row" + i;
+                container.appendChild(myRow);
+
+                let myTitle = document.createElement('td');
+                myTitle.innerHTML = String(data.films[i].title);
+                myRow.appendChild(myTitle);
+
+                let myRemoveFilm = document.createElement('td');
+                myRow.appendChild(myRemoveFilm);
+                let removeFilmbtn = document.createElement('input');
+                removeFilmbtn.type = "button";
+                removeFilmbtn.className = "btn btn-primary";
+                removeFilmbtn.value = "Remove Film";
+                removeFilmbtn.onclick = function removeFilm() {
+
+                };
+                myRemoveFilm.appendChild(removeFilmbtn);
+
+            }
+            console.log(data);
+        })
+        .catch((error) => console.log(error.message));
+    return false;
+
+
+}
