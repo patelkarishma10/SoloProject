500ecef8c9d72181b3bb7ae97648ae4f548aaf4d
Karishma Patel <patelkarishma10@gmail.com>
2019-06-18 21:55:23 +0100

    created getAUser test in UserController test

--- src/test/java/com/bae/rest/UserControllerTest.java
+++ src/test/java/com/bae/rest/UserControllerTest.java
@@ -1,12 +1,16 @@
 package com.bae.rest;
 
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import com.bae.business.service.UserService;
+import com.bae.util.Constants;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserControllerTest {
@@ -22,4 +26,9 @@ public class UserControllerTest {
 		controller.setService(service);
 	}
 
+	@Test
+	public void getAUserTest() {
+		Mockito.when(service.getAUser(1)).thenReturn(Constants.MOCK_USER_OBJECT);
+		Assert.assertEquals(Constants.MOCK_USER_OBJECT, controller.getAUser(1));
+	}
 }
